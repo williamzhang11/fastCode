60d37d85cafdb07bfbfd81ca6944565b7cb9e251
zhangxiuya <xiuyazhang@wdit.com.cn>
2019-11-28 22:20:37 +0800

    增加翻转链表

---
+++ src/main/java/com/xiu/fastCode/mycode/ReverseList.java
@@ -0,0 +1,29 @@
+package com.xiu.fastCode.mycode;
+
+
+/**
+ * 输入一个链表，反转链表后，输出新链表的表头。
+ * @author PC
+ *
+ */
+public class ReverseList {
+	
+	
+	public ListNode solution(ListNode head) {
+		
+		
+		
+		
+		return null;
+	}
+	
+	static public class ListNode {
+	    int val;
+	    ListNode next = null;
+
+	    ListNode(int val) {
+	        this.val = val;
+	    }
+	}
+
+}
