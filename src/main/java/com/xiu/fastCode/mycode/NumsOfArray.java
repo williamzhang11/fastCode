6e8b53d78206d680e8646a79e1c7654f9f5f6b72
williamzhang11 <1107517306@qq.com>
2020-01-06 23:22:33 +0800

    给定一个有n个正整数的数组A和一个整数sum,求选择数组A中部分数字和为sum的方案数

---
+++ src/main/java/com/xiu/fastCode/mycode/NumsOfArray.java
@@ -0,0 +1,12 @@
+package com.xiu.fastCode.mycode;
+//给定一个有n个正整数的数组A和一个整数sum,求选择数组A中部分数字和为sum的方案数
+//方案的数量=2个数字和为sum+3个数字和为sum+...
+//设为数字的个数为k，则1<k<=n
+//第一个数b1,范围[0,n-k],则第二个数b2，范围[b1+1,n-k+1],第三个数b3,范围[b2+1,n-k+2]...
+public class NumsOfArray {
+
+	public static void main(String[] args) {
+		
+		
+	}
+}
